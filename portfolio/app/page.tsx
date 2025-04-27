6d40d1971e1550027fc9232b19af2defeacb743d
Novus-Tech237 <novustechnologies7@gmail.com>
2025-04-27 15:28:30 +0200

    Refactor layout and styling in Home and Profil components for improved consistency and readability

--- portfolio/app/page.tsx
+++ portfolio/app/page.tsx
@@ -1,13 +1,13 @@
 import Header from "@/component/header";
-import Image from "next/image";
 
 export default function Home() {
   return (
     <div className = "h-full overflow-x-hidden  ">
 
-      <section className="min-h-[720px] bg-white w-full bg-culture ">
+      <section className="min-h-[720px] w-full ">
         <Header/>
       </section>
+      
     </div>
   );
 }
--- portfolio/component/profil.tsx
+++ portfolio/component/profil.tsx
@@ -1,27 +1,27 @@
 import Image from "next/image";
 import Hello from "./hello";
 
-const Profil = () =>{
-    return(
-        <div className="mt-5 grid grid-cols-2 w-full gap-30 justify-items-center items-center ">
+const Profil = () => {
+    return (
+        <div className="mt-5 grid grid-cols-2 w-full gap-[2rem] justify-items-center items-center ">
 
             {/*greeting speech*/}
-          <div className="flex flex-col ml-30">
-            <Hello/>
-            <p className="font-bold text-7xl ">I'm <span className="italic text-yellow-400 underline">Benoit Mbole Ateba,</span>
-           <br/>Software Engeneer<br/>Based in Germany </p>
-           <p className="mt-3">I'm a beginner Software Engeneer with 1 year in the field.</p>
-            </div>  
+            <div className="flex flex-col ml-[3rem]">
+                <Hello />
+                <p className="font-bold xl:text-7xl text-3xl ">I&apos;m <span className="italic text-yellow-400 underline">Benoit Mbole Ateba,</span>
+                    <br />Software Engeneer<br />Based in Germany </p>
+                <p className="mt-[3rem] text-muted-foreground">I&apos;m an entry level Software Engineer with 2+ Years experience in the field collaborating in various open source projects and building personal projects.</p>
+            </div>
 
             {/*HProfil photo section*/}
-           <div>
-            <Image
-            src="/Profile.png"
-            alt="This is my profile photo"
-            width={700}
-            height={300}
-            />
-           </div>
+            <div>
+                <Image
+                    src="/Profile.png"
+                    alt="This is my profile photo"
+                    width={700}
+                    height={300}
+                />
+            </div>
         </div>
     )
 }
