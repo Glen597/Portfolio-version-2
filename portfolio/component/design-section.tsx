13bea7fbeb8fe5e01ac4be96d4a0121f0c53d11d
Novus-Tech237 <novustechnologies7@gmail.com>
2025-04-29 23:04:49 +0200

    Implement code changes to enhance functionality and improve performance

--- portfolio/component/design-section.tsx
+++ portfolio/component/design-section.tsx
@@ -1,32 +1,32 @@
+import Image from "next/image";
 
 const DesignCategories = () => {
     return (
         <div className="relative z-2">
             {/* Yellow banner with design categories */}
-            <div className="bg-[#FEB33B] py-4 flex justify-center items-center  text-3xl">
-                <ul className="flex space-x-8 text-green-900 font-medium">
-                    <li className="flex items-center">
-                        App Design
-                        <span className="mx-4 text-green-900 ">*</span>
-                    </li>
-                    <li className="flex items-center">
-                        Website Design
-                        <span className="mx-4 text-green-900 ">*</span>
-                    </li>
-                    <li className="flex items-center">
-                        Dashboard
-                        <span className="mx-4 text-green-900 ">*</span>
-                    </li>
-                    <li className="flex items-center">
-                        Wireframe
-                    </li>
+            <div className="bg-[#FEB33B] py-4 flex justify-center items-center text-3xl">
+                <ul className="flex space-x-8 text-green-900 font-medium justify-around">
+                    <div className="flex justify-between items-center space-x-8">
+                        <div className="flex items-center space-x-8">
+                            <p>Web Developer</p>
+                            <Image src={"/star.png"} alt="Benoit Mbole Ateba" height={25} width={25} />
+                        </div>
+                        <div className="flex items-center space-x-8">
+                            <p>Data Analyst</p>
+                            <Image src={"/star.png"} alt="Benoit Mbole Ateba" height={25} width={25} />
+                        </div>
+                        <div className="flex items-center space-x-8">
+                            <p>Discord Bot Developer</p>
+                            <Image src={"/star.png"} alt="Benoit Mbole Ateba" height={25} width={25} />
+                        </div>
+                        <div className="flex items-center space-x-8">
+                            <p>Webscrapping</p>
+                        </div>
+                    </div>
                 </ul>
             </div>
             {/* Green inclined banner */}
-            
             <div className="absolute inset-0 -z-10 h-full bg-green-900 transform -skew-y-2"></div>
-
-            
         </div>
     );
 };
--- portfolio/component/profil.tsx
+++ portfolio/component/profil.tsx
@@ -9,7 +9,7 @@ const Profil = () => {
             <div className="flex flex-col ml-[3rem]">
                 <div>
                     <Hello />
-                    <p className="font-bold xl:text-7xl text-3xl ">I&apos;m <span className="italic text-yellow-400 underline">Benoit Mbole Ateba,</span>
+                    <p className="font-bold md:text-7xl text-3xl ">I&apos;m <span className="italic text-yellow-400 underline">Benoit Mbole Ateba,</span>
                         <br />Software Engeneer<br />Based in Germany </p>
                     <p className="mt-[3rem] text-muted-foreground">I&apos;m an entry level Software Engineer with 2+ Years experience in the field collaborating in various open source projects and building personal projects.</p>
                 </div>
