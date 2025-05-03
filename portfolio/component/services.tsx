8b652134c910d9171dffef80194fe21e8e5ca58c
Glen597 <benoitmbole4@gmail.com>
2025-05-03 15:27:12 +0200

    service boxes

--- portfolio/component/services.tsx
+++ portfolio/component/services.tsx
@@ -1,9 +1,29 @@
+'use client'
+import { Expand } from "lucide-react";
 import Image from "next/image";
 import ToggleText from "./toggleText";
+import { useState, useRef, useEffect } from "react";
+
+
+
+
+ 
 
 
 
 const ServicesPage = () =>{
+
+    const [expanded, setExpanded] = useState(false);
+    const [height, setHeight] = useState('110px'); // Hauteur fermée
+  const contentRef = useRef<HTMLParagraphElement>(null);
+
+  useEffect(() => {
+    if (contentRef.current) {
+      const fullHeight = contentRef.current.scrollHeight;
+      setHeight(expanded ? `${fullHeight}px` : '110px');
+    }
+  }, [expanded]);
+
     return(
         <div className="mt-[100px] flex flex-col">
             <div className="flex  items-center ml-[117]">
@@ -15,7 +35,7 @@ const ServicesPage = () =>{
                 />
                 <p className="ml-[2px]  text-3xl  font-sans  text-[#2A2A2A] ">Services</p>
             </div>
-            <div className="ml-[130px] flex items-center justify-between">
+            <div className="ml-[130px] flex items-center justify-between ">
                 <p className="text-7xl "><span className="text-yellow-400  ">Services</span> I Provide</p>
                  <div className="flex items-center mr-[130px]  ">
                                     <div className="flex items-center bg-[#FEB33B] rounded-full w-[200] h-12 ">
@@ -35,15 +55,18 @@ const ServicesPage = () =>{
                                 </div>
             </div>
             <div className="flex justify-between mt-[160px] mx-[130px]">
-<div className=" bg-stone-200 w-[380px] h-[370px] rounded-xl flex flex-col"> 
+<div className={` transition-all duration-500 ease-in-out rounded-xl flex flex-col  bg-stone-200 w-[380px] transform ${ expanded ? 'h-[540px] translate-y-[-20px] ' : ' h-[370px] '}` }> 
     <Image className="rounded-full relative my-[30px]  mx-[30px]"
 src="/web-developer.png"
 alt="web-developer image"
 width={80}
 height={60}/>
 <p className="font-bold text-[#3C3C3C] text-2xl mx-[20px] mb-[20px]">Web-Developer</p>
-<p className="mx-[20px] text-xl overflow-hidden line-clamp-4 font-sans ">Building modern web apps with: ⚛️React.JS<br/> 🚀Next.js<br/> 🛠️Express<br/> API integration with 📡Axios<br/>Proficient in: <br/>🧱 HTML<br/> 🎨CSS<br/> ⚙️JavaScript<br/>🔷TypeScript.</p>
-<ToggleText/>
+<p
+ref={contentRef}
+style={{ maxHeight: height }}
+className=" transition-all duration-500 ease-in-out mx-[20px] text-xl font-sans overflow-hidden  leading-relaxed ">Building modern web apps with: ⚛️React.JS<br/> 🚀Next.js<br/> 🛠️Express<br/> API integration with 📡Axios<br/>Proficient in: <br/>🧱 HTML<br/> 🎨CSS<br/> ⚙️JavaScript<br/>🔷TypeScript.</p>
+<ToggleText  expanded = {expanded} onToggle = {() => setExpanded(!expanded)}/>
 </div>
 <div className=" bg-stone-200 w-[370px] h-[350px] rounded-xl"> hello</div>
 <div className=" bg-stone-200 w-[370px] h-[350px] rounded-xl"> hello</div>
--- portfolio/component/toggleText.tsx
+++ portfolio/component/toggleText.tsx
@@ -1,16 +1,19 @@
 'use client'
 
-import { useState } from 'react';
+import React from 'react';
 
 
+interface ToggleProps{
+  expanded: boolean;
+  onToggle: ()=> void
+}
 
-  const ToggleText = () =>{
-    
-    const [expanded, setExpanded] = useState(false);
+  const ToggleText: React.FC<ToggleProps>= ({expanded, onToggle}) =>{
+  
     return(
         <div>
-            <button className={`text-[#485E4C] font-bold text-xl flex ml-[20px] mt-[20px] ${expanded ? "text-green-700" : "text-[#485E4C]"}`}
-onClick ={()=> setExpanded(!expanded)}>{expanded ? "Learn less" : "Learn more" }</button>
+            <button className={`text-[#485E4C] font-bold text-xl flex ml-[20px] mt-[20px] ${expanded ? "text-green-700 pb-[10px]" : "text-[#485E4C]"}`}
+onClick ={onToggle}>{expanded ? "Learn less" : "Learn more"} </button>
         </div>
     )
   }
