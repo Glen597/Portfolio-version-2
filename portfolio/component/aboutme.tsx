419cd529c1292bc192107895d453e583d2135d92
Glen597 <benoitmbole4@gmail.com>
2025-05-16 00:46:39 +0200

    About me

--- portfolio/component/aboutme.tsx
+++ portfolio/component/aboutme.tsx
@@ -10,16 +10,30 @@ width={700}
 height ={800}
 />
 <div className="flex flex-col items-center justify-between w-200">
-<div className="flex  items-center ">
+<div className="flex  items-center justify-items-start ml-[-100]">
                <div className="w-5 h-0.5 bg-yellow-500 mb-2"></div>
-                <p className="ml-[2px]  text-2xl  font-sans  text-[#2A2A2A] text-white ">About Me</p>
+                <p className="ml-[2px]  text-4xl  font-sans  text-[#2A2A2A] text-white  ">About Me</p>
             </div>
-            <p>Who is <span className="text">Benoit Mbole Ateba</span></p>
-            <div>div</div>
-            <div>div</div>
-            <div>div</div>
-            <div className="flex justify-between items-center">
-            <div className="flex items-center bg-[#FEB33B] rounded-full w-[170] h-12 ">
+            <p className="text-6xl font-bold text-white">Who is <span className="text italic text-yellow-400">Benoit Mbole Ateba</span></p>
+            <div className="text-xl text-white mt-[50px]">Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                 Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+                  when an unknown printer took a galley of type and scrambled it to make a type specimen book</div>
+                   <div className="text-xl flex gap-x-20">
+                    <div className="flex flex-col">
+                         <p className="text-yellow-400 mt-[50px] text-5xl font-bold">10+</p>
+                         <p className="text-xl text-white">Project Completed</p>
+                    </div>
+                    <div className="flex flex-col">
+                            <p className="text-yellow-400 mt-[50px] text-5xl font-bold">20+</p>
+                         <p className="text-xl text-white">Industry Covered</p>
+                    </div>
+                    <div className="flex flex-col">
+                            <p className="text-yellow-400 mt-[50px] text-5xl font-bold">1+</p>
+                         <p className="text-xl text-white">Years Of experience</p>
+                    </div>
+                   </div>
+           <div className="flex justify-between items-center mt-[50px] ">
+            <div className="flex items-center bg-[#FEB33B] rounded-full w-[170] h-12  ">
                                     <button className="rounded-full bg-green-900 px-3 my-6 text-white py-[10] ml-[2] mt-[26]">Download cv</button>
                                     <a className="bg-white rounded-full ml-[9] ">
                                         <Image
@@ -30,7 +44,7 @@ height ={800}
                                         />
                                     </a>
                                 </div>
-                                <p>Benoit Mbole Ateba</p>
+                                <p className="ml-[100]">Benoit Mbole Ateba</p>
             </div>
                  
             
